480603171415c2aa79e9fdcaa61b7fa4bc9b7e1c
emaozdemir <semaozdemiraslan@gmail.com>
2025-01-03 19:36:34 +0100

    general notes

---
+++ src/general_notes/prep01.java
@@ -0,0 +1,16 @@
+package general_notes;
+
+public class prep01 {
+    /*
+    Metod nedir?  • Bir işlemi gerçekleştirmek için bir araya getirilen ifadeler kümesidir. Örneğin,
+    System.out.println()  metodunu çağırdığınızda, sistem, konsolda bir mesaj görüntülemek için birkaç ifadeyi sırayla yürütür.
+    Bir metod, adıyla belirtilen ve programın herhangi bir noktasında çağrılabilen (çalıştırılabilen) kod  setidir.
+    Bir metod, veri üzerinde işlem yapan
+    ve genellikle bir değer döndüren bir alt program olarak  düşünülebilir. Her metodun kendi adı vardır.
+
+    Constructor nedir?  • Java'da bir constructor , nesneleri başlatmak için kullanılan özel bir yöntemdir.
+    Constructor, bir  classın bir nesnesi oluşturulduğunda çağrılır.  • Bir nesne new() anahtar kelimesi
+    kullanılarak oluşturulduğunda,
+    en az bir constructor (varsayılan  constructor olabilir) classın veri üyelerine başlangıç değerleri atamak için çağrılır.
+     */
+}
